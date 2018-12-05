386711f6672e95b1cb7b58873b984bbf3f0c3d8a
Caitlyn Grudzinski <cgrudzin@chicagobooth.edu>
2018-12-05 21:17:44 +0000

    update date formats on index page

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-gmail_username: 'practiceproductmanagement@gmail.com'
-gmail_password: 'appdev123'
+#gmail_username: 'practiceproductmanagement@gmail.com'
+#gmail_password: 'appdev123'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
