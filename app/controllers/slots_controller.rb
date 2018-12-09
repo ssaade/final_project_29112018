3656c2c440048b260a0891faaf6abb22a987fc86
Caitlyn Grudzinski <cgrudzin@chicagobooth.edu>
2018-12-09 20:34:22 +0000

    bin update and match logic

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -36,8 +36,8 @@
   def create_row
     @slot = Slot.new
     @slot.user_id = params.fetch("user_id")
-    @slot.start_time = params.fetch("start_time")
-    @slot.end_time = params.fetch("end_time")
+    @slot.start_time = DateTime.strptime(params.fetch("start_time"), "%m/%d/%Y %l:%M %P")
+    @slot.end_time = DateTime.strptime(params.fetch("end_time"), "%m/%d/%Y %l:%M %P")
     
     if @slot.valid?
       @slot.save
@@ -79,19 +79,50 @@
 
   def edit_form
     @slot = Slot.find(params.fetch("prefill_with_id"))
-
+    
     render("slot_templates/edit_form.html.erb")
   end
 
   def update_row
     @slot = Slot.find(params.fetch("id_to_modify"))
-
     
     @slot.start_time = params.fetch("start_time")
     @slot.end_time = params.fetch("end_time")
 
     if @slot.valid?
       @slot.save
+      
+    #Remove all matches that have this current slot
+    Match.where(sender_slot_id: current_user.id).delete_all
+    Match.where(recipient_slot_id: current_user.id).delete_all
+    
+    slot_list = Slot.all
+      
+      slot_list.each do |candidate_slot|
+        
+        if candidate_slot.user_id != current_user.id && candidate_slot.start_time.to_date == @slot.start_time.to_date
+          
+            @match = Match.new
+            
+            @match.sender_slot_id = @slot.id
+            @match.recipient_slot_id = candidate_slot.id
+            
+            if @slot.start_time > candidate_slot.start_time
+              @match.start_time = @slot.start_time
+            else
+              @match.start_time = candidate_slot.start_time            
+            end
+            
+            if @slot.end_time < candidate_slot.end_time
+              @match.end_time = @slot.end_time
+            else
+              @match.end_time = candidate_slot.end_time            
+            end
+            
+            @match.save
+          
+        end
+      end
 
       redirect_to("/slots/#{@slot.id}", :notice => "Slot updated successfully.")
     else
